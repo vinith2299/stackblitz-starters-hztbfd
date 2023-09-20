b3562e59f777fc464a98b15882433bb297e1cdf2
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-20 16:48:35 +0530

    1 file modified, 1 added

--- src/pages/pagination.jsx
+++ src/pages/pagination.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import '../css/pagination.css';
 
 function Pagination() {
-  return <div>Pagination</div>;
+  return <div><table>
+  <tr>
+      <th>postid</th>
+      <th>id</th>
+      <th>name</th>
+      <th>email</th>
+      <th>body</th>
+  </tr>
+  <tr>
+      <td>1</td>
+      <td>1</td>
+      <td> vinith</td>
+      <td>vinith2299@gmail.com</td>
+      <td> urhhubghfwhfeneuihberubryvbruijh</td>
+  </tr>
+  <tr>
+      <td>2</td>
+      <td>2</td>
+      <td>vedyut</td>
+      <td>vedyut2299@gmail.com</td>
+      <td> udbubufwhfeneuihberubryvbruijh</td>
+  </tr>
+</table></div>;
 }
 
 export default Pagination;
